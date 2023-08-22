0d8353d2e2f89fe2150dec3a5f8e523949e0b05d
pcadmin <luanabnitsche@gmail.com>
2023-08-22 16:31:12 -0300

    changing print

--- ex012.py
+++ ex012.py
@@ -4,8 +4,8 @@ def saudacao(aluno):
 nome = input("Qual o seu nome? ")
 saudacao(nome)
 
-def soma(x, y):
-    print("A soma é ", x + y)
+def soma(v1, v2):
+    print("A soma entre", str(x), "e", str(y), "é", v1 + v2)
 
 x = int(input("Valor de x: "))
 y = int(input("Valor de y: "))
