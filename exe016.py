5e37c227a0e7007ca9b0dbec8e012aab7eba13f9
pcadmin <luanabnitsche@gmail.com>
2023-08-24 14:53:16 -0300

    alteracao

--- exe016.py
+++ exe016.py
@@ -1,7 +1,8 @@
 
 from num2words import num2words
-num_list = [0]
+num_list = [5]
 for num in range (40, 51):
     num_in_words = num2words(num)
     num_list.append(num_in_words)
 print(num_list)
+print(num2words(405023, lang = 'sl'))
