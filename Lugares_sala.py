73d64caa066e31ac5c788523d872e7589b0669e4
pcadmin <luanabnitsche@gmail.com>
2023-08-24 14:17:55 -0300

    lista

---
+++ Lugares_sala.py
@@ -0,0 +1,34 @@
+import random
+alunos = ["Ademir Batschauer Junior",
+    "Alex Sandro Rodrigues Mette",
+    "Angelo Miguel Requenha",
+    "Arthur Henrique Erhardt",
+    "Bernardo Martins Fontaina",
+    "Bianca Lanser Peres",
+    "Cristina Siewert Jansen",
+    "Daianna Marques Dos Santos",
+    "Daniel Nunes",
+    "Daniele Karine Modesto De Araujo",
+    "Fábio Henrique Peixe",
+    "Felipe Batista Dos Santos",
+    "Guilherme Bueno Zago",
+    "Isabela Caroline Reiter",
+    "Ivanir Stano",
+    "Júlia Francine De Oliveira",
+    "Marco Aurelio Ribeiro Martins",
+    "Matheus Santos Novak",
+    "Ryan Wessling Da Silva",
+    "Victor Gabriel",
+    "Victor Gomes Mendes",
+    "Victor Luís Zuchi",
+    "William Gonçalves Soares"
+    ]
+
+while alunos:
+    
+    for i in range(23):
+        
+        aluno_escolhido = random.choice(alunos) #randomiza a escolha dentro da lista 
+        x = alunos.remove(aluno_escolhido)
+        print(i, aluno_escolhido)
+    break
