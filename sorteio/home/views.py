2d81c67bef06ceb0ebe06b44a58f1545450a34a6
LuanaNitsche <126619742+LuanaNitsche@users.noreply.github.com>
2023-08-29 13:30:43 -0300

    Delete sorteio directory

--- sorteio/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-def sorteio_view(request):
-    # Coloque o código relacionado ao sorteio aqui
-    # Pode ser o código para realizar o sorteio ou apenas renderizar um template
-    return render(request, 'nome_do_template.html')  # Lembre-se de criar o template HTML
