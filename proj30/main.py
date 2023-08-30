085b699d62b80e0bfd4457cd97fe3bf5a6517c4a
pcadmin <luanabnitsche@gmail.com>
2023-08-30 15:33:56 -0300

    abc

--- proj30/main.py
+++ proj30/main.py
@@ -1,15 +1,16 @@
 from random_word import RandomWords
+from translate import Translator
 
-word = RandomWords() #variavel word recebe classe Randomword
+word = RandomWords()
+translator = Translator(to_lang="pt")
 
 while True:
-    random = word.get_random_word() #variavel random recebe variavel word no metodo palavra aleatoria
+    random_word = word.get_random_word()
     
-    if len(random) >= 8:
-        print(random)
-        random = word.get_random_word()
-        print(random)
+    if len(random_word) >= 8:
+        traduzida = translator.translate(random_word)
+        print("Palavra original em inglês:", random_word)
+        print("Palavra traduzida para o português:", traduzida)
         break
     else:
-        print("A palavra não encontrada.")
-
+        print("A palavra não atende aos requisitos de comprimento.")
