30db1d88877b7be07890ccd1b5b683d33f6c8541
pcadmin <luanabnitsche@gmail.com>
2023-08-30 14:53:49 -0300

    maluquice

--- proj30/main.py
+++ proj30/main.py
@@ -1,9 +1,9 @@
 from random_word import RandomWords
 
-word = RandomWords()
+word = RandomWords() #variavel word recebe classe Randomword
 
 while True:
-    random = word.get_random_word()
+    random = word.get_random_word() #variavel random recebe variavel word no metodo palavra aleatoria
     
     if len(random) >= 8:
         print(random)
