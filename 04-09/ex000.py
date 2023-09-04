9fa3435d70e31c448c960497894eb4159c20600e
pcadmin <luanabnitsche@gmail.com>
2023-09-04 14:35:54 -0300

    calul

--- 04-09/ex000.py
+++ 04-09/ex000.py
@@ -1,5 +1,12 @@
-from mydefs import entra21, name
+from mydefs import entra21, name, calculo
 from env import API_KEY
 
-print(name)
-print(entra21('alcol'))
+
+def main():
+    print(name)
+    print(entra21(name))
+    print(calculo(30, 45))
+
+if __name__ == '__main__':
+    main()
+
--- 04-09/mydefs.py
+++ 04-09/mydefs.py
@@ -1,7 +1,13 @@
-name = {'Laura '
-        'Rodrigo '
+name = {'Laura, '
+        'Rodrigo, '
         'Fernanda '}
 
-def entra21(word):
-    ret = str(word)
+def entra21(name):
+    ret = str(name)
     return ret.upper()
+
+def calculo(x, y):
+    return print("A divisão entre", str(x), "e", str(y), "é", x / y)
+   
+
+
