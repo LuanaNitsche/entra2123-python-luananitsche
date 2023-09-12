eca6786cce8b18d6c74080bd57d2bdf846b96c73
pcadmin <luanabnitsche@gmail.com>
2023-09-12 15:29:34 -0300

    html

--- Django/django_04/angelica/urls.py
+++ Django/django_04/angelica/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import helloworld
+from .views import helloworld, index
+
+
 
 urlpatterns = [  
     path('', helloworld, name= 'helloworld'),
+    path('i', index, name= 'index'),
 ]
--- Django/django_04/angelica/views.py
+++ Django/django_04/angelica/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def helloworld(request):
-    return HttpResponse('Hello world, my brain is melting')
+    return render(request, 'angelica/index.html')
+
+def index(request):
+    context = {
+        'minha_string' : 'Olá mundo',
+        'meu_inteiro' : 123,
+        'meu_booleano' : True,
+        'meu_lista' : [1,2,3,4,5],
+    }
+    return render(request, 'angelica/index2.html', context)
