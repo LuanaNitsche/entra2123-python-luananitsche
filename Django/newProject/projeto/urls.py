00428c9d553a32c7d1ef3899dde58e8e591aaa7d
LuanaNitsche <luanabnitsche@gmail.com>
2023-09-21 11:43:32 -0300

    aula segunda

--- Django/newProject/projeto/urls.py
+++ Django/newProject/projeto/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include ('robertinha.urls'))
+    path('', include ('robertinha.urls')),
+    path('bea/', include ('beatrice.urls')),
 ]
