dd562aafff73d4beb4091f88a56822ce7d6f63a8
pcadmin <luanabnitsche@gmail.com>
2023-10-02 16:48:26 -0300

    alegria

--- Django/newProject/robertinha/views.py
+++ Django/newProject/robertinha/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse
 from .forms import ContatoForm, Ex003Form, QuestionForm
-
+from datetime import datetime
+import locale
 
 def ex002(request):
     context = {
@@ -271,9 +272,27 @@ def mercado(request):
         pass
     else:
         produtos ={
-            '1':{'nome':'Arroz','id_for' : '11','id_cat' : '202', 'id_estoque' : '02'},
-            '2':{'nome':'Papel higiênico','id_for' : '22','id_cat' : '404', 'id_estoque' : '01'},        
-            '3':{'nome':'MI9','id_for' : '33','id_cat' : '505', 'id_estoque' : '03'}
+            '1':{'nome':'Arroz',
+                 'id_for' : '11',
+                 'id_cat' : '202', 
+                 'id_estoque' : '02', 
+                 'id_vcto' : datetime(2022, 10, 5), 
+                 'dias_vencidos' : calculo_vencimento(datetime(2022, 10, 5))},
+
+            '2':{'nome':'Papel higiênico',
+                 'id_for' : '22',
+                 'id_cat' : '404', 
+                 'id_estoque' : '01', 
+                 'id_vcto' : datetime(2023, 10, 2), 
+                 'dias_vencidos' : calculo_vencimento(datetime(2023, 10, 2))},        
+
+            '3':{'nome':'MI9',
+                 'id_for' : '33',
+                 'id_cat' : '505', 
+                 'id_estoque' : '03', 
+                 'id_vcto' : '030', 
+                 'id_vcto' : datetime(2023, 10, 5),
+                 'dias_vencidos' : calculo_vencimento(datetime(2023, 10, 5))}
         }
         
         fornecedores = {
@@ -293,31 +312,67 @@ def mercado(request):
             '02' : {'localizacao' : 'Prateleira A5', 'saldo' : '0'},
             '03' : {'localizacao' : 'Prateleira D2', 'saldo' : '10'}
         }
-        
+
         novoDic = {}
-    
-        for id_produtos, info in produtos.items(): ##id_produtos = chave, info = dic de info de produto
-            id_for = info['id_for']                ##pega id do fornecedor
-            id_cat = info['id_cat']                ##pega id da categoria
-            id_estoque = info ['id_estoque']
+        lista_dias_vencidos = []                   #inicia a lista para futuro armazenamento dos dias
+        
+        for id_produtos, info in produtos.items(): #id_produtos = chave, info = dic de info de produto
+            id_for = info['id_for']                #pega id do fornecedor
+            id_cat = info['id_cat']                #pega id da categoria
+            id_estoque = info ['id_estoque']       #pega id do fornecedor
+            id_vcto = info['id_vcto']
+        
             
             
-            nome_prod = info['nome']               ##pega o nome do produto a partir da info
+            nome_prod = info['nome']
+            dias_vencidos = calculo_vencimento(info['id_vcto'])           #pega o nome do produto a partir da info
+            
+            lista_dias_vencidos.append(dias_vencidos)                     #armazena os dias vencidos de cada produto dentro de uma lista
+            
             nome_for = fornecedores.get(id_for, {}).get('nome', 'Desconhecida')     #pega o nome do fornecedor com base na id dele
             nome_cat = categoria.get(id_cat, {}).get('nome', 'Desconhecida')        #pega o nome da categoria com base na id dela
-            local = estoque.get(id_estoque, {'localizacao' : 'Desconhecida', 'saldo' : '0'})
-            novoDic[id_produtos] = {               #popula o novo dicionario que junta todas as informações
+            local = estoque.get(id_estoque, {'localizacao' : 'Desconhecida', 'saldo' : '0'}) #pega a localizacao e o saldo com base no id do estoque
+            novoDic[id_produtos] = {               #popula o novo dicionario com as infos que coletamos acima com o .get
                 'Produto' : nome_prod,        #associa a chave (nome_produto) ao nome no dicionario (nome_prod)
-                'Fornecedor' : nome_for,      #faz o mesmo com fornecedor e categoria
+                'Fornecedor' : nome_for,      #faz o mesmo com fornecedor, categoria, localizacao, saldo, vencimento, media, datavalidade
                 'Categoria' : nome_cat,
                 'Localização' : local['localizacao'],
-                'Saldo' : local['saldo']
+                'Saldo' : local['saldo'],
+                'DataValidade': formatar_data_brasileira(id_vcto),
+                'Vencimento' : dias_vencidos,
+                'MediaVencimento' : dias_vencidos
             }
+        if lista_dias_vencidos:
+            media_dias_vencidos = sum(lista_dias_vencidos) / len(lista_dias_vencidos)       #cria var para armazenar a soma dos dias dividido pela quantidade de dias armazenados na lista
+            media_dias_vencidos = round(media_dias_vencidos, 2)                             #formata o número para 2 digitos após a vírgula
+            if media_dias_vencidos < 0:                                                     #deixa os dias no formato positivo
+                media_dias_vencidos = abs(media_dias_vencidos)
+        else:
+            media_dias_vencidos = 0                                                         #se a lista estiver vazia, retorna vazio
         
-        #print(novoDic)   
+        print(novoDic)   
     
-        return render(request, 'robertinha/ex006.html', {'novoDic': novoDic})
+        return render(request, 'robertinha/ex006.html', {'novoDic': novoDic, 'media_dias_vencidos': media_dias_vencidos})  #imprime o novoDicionario e a media de vencimento
   
   
-  
+def calculo_vencimento(id_vcto):
+    data_vcto = id_vcto                         #recebe a data de validade
+    data_hoje = datetime.now()                  #recebe a data de hoje
+
+    if data_vcto <= data_hoje:                  #compara se a data de validade é menor que a data de hoje
+        dias_vencidos = (data_vcto - data_hoje).days        #guarda o resultado da subtracao na var dias_vencidos
+        dias_vencidos = abs(dias_vencidos)                  #deixa no formato positivo
+    else:
+        dias_vencidos = 0  # Se a data de validade já passou, definimos dias_vencidos como 0
+    return dias_vencidos
+
+
+
+def formatar_data_brasileira(data):
+    if data is None:
+        return ""
+    return data.strftime("%d/%m/%Y")
+
+
+
   
